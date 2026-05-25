bbac1cb3ec4bbd6e48d18f4fddae4543f293a148
harshparmar009 <hardinmartin09@gmail.com>
2026-05-25 16:11:32 +0530

    fixing admin firebase

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,23 +1,42 @@
 import admin from "firebase-admin";
 import { createRequire } from "module";
 
+
+console.log("ENV CHECK:", {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKeyExists: !!process.env.FIREBASE_PRIVATE_KEY,
+  nodeEnv: process.env.NODE_ENV,
+});
+
+// if (!admin.apps.length) {
+//   //  Use JSON file locally, env variables in production
+//   if (process.env.NODE_ENV === "production") {
+//     admin.initializeApp({
+//       credential: admin.credential.cert({
+//         projectId: process.env.FIREBASE_PROJECT_ID,
+//         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+//       }),
+//     });
+//   // } else {
+//   //   const require = createRequire(import.meta.url);
+//   //   const serviceAccount = require("./serviceAccountKey.json");
+//   //   admin.initializeApp({
+//   //     credential: admin.credential.cert(serviceAccount),
+//   //   });
+//   }
+// }
+
 if (!admin.apps.length) {
-  //  Use JSON file locally, env variables in production
-  if (process.env.NODE_ENV === "production") {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
-    });
-  // } else {
-  //   const require = createRequire(import.meta.url);
-  //   const serviceAccount = require("./serviceAccountKey.json");
-  //   admin.initializeApp({
-  //     credential: admin.credential.cert(serviceAccount),
-  //   });
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+  console.log("✅ Firebase Admin initialized");
 }
 
 export default admin;
