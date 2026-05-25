aac39eac8cc245d07dfec7a32e8e4147193f957e
harshparmar009 <hardinmartin09@gmail.com>
2026-05-25 13:39:40 +0530

    cors problem

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const MAX_FAILURES = 3;
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
   },
 });
