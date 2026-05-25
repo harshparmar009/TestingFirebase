0fb7af6efa7264a837a641aa5a526055ef0bd81d
harshparmar009 <hardinmartin09@gmail.com>
2026-05-25 16:29:57 +0530

    adminfirebase problem

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,6 +1,37 @@
-import admin from "firebase-admin";
-import { createRequire } from "module";
+// import admin from "firebase-admin";
+// import { createRequire } from "module";
 
+
+// console.log("ENV CHECK:", {
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//   privateKeyExists: !!process.env.FIREBASE_PRIVATE_KEY,
+//   nodeEnv: process.env.NODE_ENV,
+// });
+
+// if (!admin.apps.length) {
+//   //  Use JSON file locally, env variables in production
+//   if (process.env.NODE_ENV === "production") {
+//     admin.initializeApp({
+//       credential: admin.credential.cert({
+//         projectId: process.env.FIREBASE_PROJECT_ID,
+//         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+//       }),
+//     });
+//   // } else {
+//   //   const require = createRequire(import.meta.url);
+//   //   const serviceAccount = require("./serviceAccountKey.json");
+//   //   admin.initializeApp({
+//   //     credential: admin.credential.cert(serviceAccount),
+//   //   });
+//   }
+// }
+
+// export default admin;
+
+
+import admin from "firebase-admin";
 
 console.log("ENV CHECK:", {
   projectId: process.env.FIREBASE_PROJECT_ID,
@@ -10,8 +41,7 @@ console.log("ENV CHECK:", {
 });
 
 if (!admin.apps.length) {
-  //  Use JSON file locally, env variables in production
-  if (process.env.NODE_ENV === "production") {
+  try {
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
@@ -19,24 +49,10 @@ if (!admin.apps.length) {
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       }),
     });
-  // } else {
-  //   const require = createRequire(import.meta.url);
-  //   const serviceAccount = require("./serviceAccountKey.json");
-  //   admin.initializeApp({
-  //     credential: admin.credential.cert(serviceAccount),
-  //   });
+    console.log("✅ Firebase Admin initialized");
+  } catch (err) {
+    console.error("❌ Firebase Admin init failed:", err.message);
   }
 }
-
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.FIREBASE_PROJECT_ID,
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-//     }),
-//   });
-//   console.log("✅ Firebase Admin initialized");
-// }
 
 export default admin;
